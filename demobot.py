56b9ddf5e92a364705238053a974e7c18c36e3b3
Ellie Yang <eyan5280@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:40:31 +1100

    add greet and ncss page

--- demobot.py
+++ demobot.py
@@ -1,10 +1,19 @@
-from flask import Flask 
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return ' Hello, World!'
+
+@app.route('/ncss')
+def ncss():
+    return '<h1>ncss</h1>'
+
+@app.route('/greet')
+def greet_person():
+    name = request.values.get('name')
+    return f'hi {name}!'
 
 if __name__ == '__main__':
     app.run()

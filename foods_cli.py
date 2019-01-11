33cebedafff8e5067ed9033d69ea72aff64fc0e2
Ellie Yang <eyan5280@SHARED.SYDNEY.EDU.AU>
2019-01-11 17:06:36 +1100

    configure it for slack

--- foods_cli.py
+++ foods_cli.py
@@ -12,17 +12,16 @@ def hello_world():
 def safe_foods():
     state = 'NO_QUERY'
     context = {}
-
+    output_string  = ''
     output = foods.ON_ENTER_STATE[state](context)
-    print(output)
+    output_string += output
     
-    while True:
     
-        line = request.values.get('text')
-        ret = foods.ON_INPUT[state](line, context)
-        state, context, optional_output = ret
-        if optional_output:
-            print(optional_output)
+    line = request.values.get('text')
+    ret = foods.ON_INPUT[state](line, context)
+    state, context, optional_output = ret
+    if optional_output:
+        output_string += optional_output
 
-        output = foods.ON_ENTER_STATE[state](context)
-        print(output)
+    output_string += foods.ON_ENTER_STATE[state](context)
+    return(output_string)
